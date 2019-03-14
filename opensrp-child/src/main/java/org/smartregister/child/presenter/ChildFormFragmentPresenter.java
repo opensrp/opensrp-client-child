644e9d6f1ec9784f8a6aa6f7548b96e53ea38389
Martin Ndegwa <ndegwamartin@gmail.com>
2019-03-14 12:54:46 +0300

    Fis issue registration form crashes on certain devices Bump up library versions

--- opensrp-child/src/main/java/org/smartregister/child/presenter/ChildFormFragmentPresenter.java
+++ opensrp-child/src/main/java/org/smartregister/child/presenter/ChildFormFragmentPresenter.java
@@ -1,13 +1,9 @@
 package org.smartregister.child.presenter;
 
-import android.widget.LinearLayout;
-
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interactors.JsonFormInteractor;
 import com.vijay.jsonwizard.presenters.JsonFormFragmentPresenter;
-import com.vijay.jsonwizard.utils.ValidationStatus;
 
-import org.smartregister.child.fragment.ChildFormFragment;
 import org.smartregister.child.util.Constants;
 
 /**
@@ -26,18 +22,6 @@ public class ChildFormFragmentPresenter extends JsonFormFragmentPresenter {
         super.setUpToolBar();
     }
 
-    @Override
-    public void onNextClick(LinearLayout mainView) {
-        ValidationStatus validationStatus = this.writeValuesAndValidate(mainView);
-        if (validationStatus.isValid()) {
-            JsonFormFragment next = ChildFormFragment.getFormFragment(mStepDetails.optString(Constants.JSON_FORM_EXTRA.NEXT));
-            getView().hideKeyBoard();
-            getView().transactThis(next);
-        } else {
-            validationStatus.requestAttention();
-            getView().showToast(validationStatus.getErrorMessage());
-        }
-    }
 
     public boolean intermediatePage() {
         return this.mStepDetails != null && this.mStepDetails.has(Constants.JSON_FORM_EXTRA.NEXT);
