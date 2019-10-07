30194d3e6c9710f39640424614e91335f43f0e56
Martin Ndegwa <ndegwamartin@gmail.com>
2019-10-07 15:08:54 +0300

    Fix unit tests

--- opensrp-child/src/test/java/org/smartregister/child/task/VaccinationAsyncTaskTest.java
+++ opensrp-child/src/test/java/org/smartregister/child/task/VaccinationAsyncTaskTest.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
@@ -14,9 +15,15 @@ import org.smartregister.child.ChildLibrary;
 import org.smartregister.child.domain.RegisterActionParams;
 import org.smartregister.immunization.ImmunizationLibrary;
 import org.smartregister.immunization.db.VaccineRepo;
+import org.smartregister.immunization.domain.jsonmapping.Vaccine;
+import org.smartregister.immunization.domain.jsonmapping.VaccineGroup;
 import org.smartregister.util.AppProperties;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -29,11 +36,52 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
     private VaccinationAsyncTask vaccinationAsyncTask;
     private ImmunizationLibrary immunizationLibrary;
 
+    @Mock
+    private Context context;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
 
         immunizationLibrary = Mockito.mock(ImmunizationLibrary.class);
+
+        Map<String, Object> vaccineRepoMap = new HashMap<>();
+
+        List<Vaccine> groupVaccines = new ArrayList<>();
+        Vaccine vaccine;
+
+        vaccine = new Vaccine();
+        vaccine.setName("opv0");
+        groupVaccines.add(vaccine);
+
+        vaccine = new Vaccine();
+        vaccine.setName("opv1");
+        groupVaccines.add(vaccine);
+
+        vaccine = new Vaccine();
+        vaccine.setName("bcg");
+        groupVaccines.add(vaccine);
+
+        vaccine = new Vaccine();
+        vaccine.setName("0pv2");
+        groupVaccines.add(vaccine);
+
+        VaccineGroup vaccineGroup = Mockito.spy(VaccineGroup.class);
+        vaccineGroup.vaccines = groupVaccines;
+
+        vaccineRepoMap.put("vaccines.json", Arrays.asList(new VaccineGroup[]{vaccineGroup}));
+
+        Mockito.when(immunizationLibrary.getVaccinesConfigJsonMap()).thenReturn(vaccineRepoMap);
+
+
+        HashMap<String, String> vaccineGrouping = new HashMap<>();
+        vaccineGrouping.put("bcg", "At Birth");
+        vaccineGrouping.put("opv0", "6 weeks");
+        vaccineGrouping.put("opv1", "10 weeks");
+        vaccineGrouping.put("opv2", "14 weeks");
+
+        Mockito.doReturn(vaccineGrouping).when(immunizationLibrary).getVaccineGroupings(Mockito.any(Context.class));
+
         ReflectionHelpers.setStaticField(ImmunizationLibrary.class, "instance", immunizationLibrary);
 
         ChildLibrary childLibrary = Mockito.mock(ChildLibrary.class);
@@ -52,23 +100,15 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
     }
 
     @After
-    public void tearDown() throws Exception {
-        ReflectionHelpers.setStaticField(ChildLibrary.class, "instance", (ChildLibrary) null);
-        ReflectionHelpers.setStaticField(ImmunizationLibrary.class, "instance", (ImmunizationLibrary) null);
+    public void tearDown() {
+        ReflectionHelpers.setStaticField(ChildLibrary.class, "instance", null);
+        ReflectionHelpers.setStaticField(ImmunizationLibrary.class, "instance", null);
     }
 
     @Test
     public void getGroupNameShouldCallImmunizationLibrary() {
-        HashMap<String, String> vaccineGrouping = new HashMap<>();
-        vaccineGrouping.put("bcg", "At Birth");
-        vaccineGrouping.put("opv0", "6 weeks");
-        vaccineGrouping.put("opv1", "10 weeks");
-        vaccineGrouping.put("opv2", "14 weeks");
 
-        Mockito.doReturn(vaccineGrouping).when(immunizationLibrary).getVaccineGroupings(Mockito.any(Context.class));
-
-        assertEquals("14 weeks", ReflectionHelpers.callInstanceMethod(vaccinationAsyncTask
-                , "getGroupName"
+        assertEquals("14 weeks", ReflectionHelpers.callInstanceMethod(vaccinationAsyncTask, "getGroupName"
                 , ReflectionHelpers.ClassParameter.from(VaccineRepo.Vaccine.class, VaccineRepo.Vaccine.opv2)));
         Mockito.verify(immunizationLibrary, Mockito.times(1))
                 .getVaccineGroupings(Mockito.any(Context.class));
@@ -76,6 +116,7 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
 
     @Test
     public void localizeStateKeyShouldReturn6weeksWhenGivenMixedCase6WeeksStateKey() {
+
         String localizedStateKey = ReflectionHelpers.callInstanceMethod(vaccinationAsyncTask
                 , "localizeStateKey"
                 , ReflectionHelpers.ClassParameter.from(String.class, "6 WEEKS"));
