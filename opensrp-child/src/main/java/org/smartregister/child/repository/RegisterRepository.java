8793ed302d35f6adacc95630d098791778042b11
bennsimon <botieno@ona.io>
2020-02-03 17:34:26 +0300

    update main condition

--- opensrp-child/src/main/java/org/smartregister/child/repository/RegisterRepository.java
+++ opensrp-child/src/main/java/org/smartregister/child/repository/RegisterRepository.java
@@ -10,7 +10,7 @@ public class RegisterRepository {
                 "mother.last_name as mother_last_name," + getDemographicTable() + ".dob, mother.dob as mother_dob, mother_details.nrc_number as mother_nrc_number, mother_details.father_name, mother_details.epi_card_number," +
                 "" + getDemographicTable() + ".client_reg_date,child_details.pmtct_status, " + getDemographicTable() + ".last_interacted_with,\n" +
                 " child_details.inactive, child_details.lost_to_follow_up, child_details.contact_phone_number from " + getChildDetailsTable() + " child_details\n" +
-                "join " + getMotherDetailsTable() + " mother_details on child_details.relational_id = mother_details.base_entity_id join " + getDemographicTable() + " ec_client on " +
+                "join " + getMotherDetailsTable() + " mother_details on child_details.relational_id = mother_details.base_entity_id join " + getDemographicTable() + " on " +
                 "" + getDemographicTable() + ".base_entity_id=child_details.base_entity_id join " + getDemographicTable() + " mother\n" +
                 "on mother.base_entity_id = mother_details.base_entity_id";
     }
