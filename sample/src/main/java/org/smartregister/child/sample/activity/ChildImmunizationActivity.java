a0464b646d5d8367c6de14d87be8b4e3c0df75b0
Ephraim Kigamba <ekigamba@ona.io>
2020-02-19 13:25:15 +0300

    Change references to toolbar.getCurrentLocation

--- sample/src/main/java/org/smartregister/child/sample/activity/ChildImmunizationActivity.java
+++ sample/src/main/java/org/smartregister/child/sample/activity/ChildImmunizationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import org.apache.commons.lang3.tuple.Triple;
+import org.smartregister.AllConstants;
 import org.smartregister.child.activity.BaseChildImmunizationActivity;
 import org.smartregister.child.domain.RegisterClickables;
 import org.smartregister.child.sample.application.SampleApplication;
@@ -48,8 +49,7 @@ public class ChildImmunizationActivity extends BaseChildImmunizationActivity {
 
         Intent intent = new Intent(fromContext, ChildDetailTabbedActivity.class);
         Bundle bundle = new Bundle();
-        bundle.putString(Constants.INTENT_KEY.LOCATION_ID,
-                LocationHelper.getInstance().getOpenMrsLocationId(getCurrentLocation()));
+        bundle.putString(Constants.INTENT_KEY.LOCATION_ID, org.smartregister.child.util.Utils.context().allSharedPreferences().getPreference(AllConstants.CURRENT_LOCATION_ID));
         bundle.putSerializable(Constants.INTENT_KEY.EXTRA_CHILD_DETAILS, childDetails);
         bundle.putSerializable(Constants.INTENT_KEY.EXTRA_REGISTER_CLICKABLES, registerClickables);
         intent.putExtras(bundle);
