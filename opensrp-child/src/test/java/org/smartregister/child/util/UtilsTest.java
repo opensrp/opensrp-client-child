db1887ed755c54518e9564de6e5732180020ff6f
Rodgers Andati <andatirodgers@gmail.com>
2020-05-05 11:53:14 +0300

    test Utils

--- opensrp-child/src/test/java/org/smartregister/child/util/UtilsTest.java
+++ opensrp-child/src/test/java/org/smartregister/child/util/UtilsTest.java
@@ -11,12 +11,15 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.smartregister.Context;
 import org.smartregister.child.ChildLibrary;
 import org.smartregister.clientandeventmodel.FormEntityConstants;
 import org.smartregister.domain.UniqueId;
 import org.smartregister.repository.UniqueIdRepository;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ChildLibrary.class)
@@ -26,6 +29,9 @@ public class UtilsTest {
 
     @Mock
     private UniqueIdRepository uniqueIdRepository;
+
+    @Mock
+    private Context context;
 
     @Before
     public void setUp() {
@@ -59,5 +65,64 @@ public class UtilsTest {
         String testString = "3929389829839829839835";
         String formattedIdentifier = Utils.formatIdentifiers(testString);
         Assert.assertEquals("3929-3898-2983-9829-8398-35",formattedIdentifier);
+    }
+
+    @Test
+    public void testPutAll() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1","one");
+        map.put("2","two");
+
+        Map<String, String> extend = new HashMap<>();
+        extend.put("3","three");
+        extend.put("4", null);
+
+        Utils.putAll(map, extend);
+        Assert.assertEquals(3, map.size());
+    }
+
+    @Test
+    public void testFormatNumber() {
+        String s1 = Utils.formatNumber("1234W");
+        String s2 = Utils.formatNumber("01234");
+        String s3 = Utils.formatNumber(" 1234");
+
+        Assert.assertEquals("1234", s1);
+        Assert.assertEquals("1234", s2);
+        Assert.assertEquals(" 1234", s3);
+    }
+
+    @Test
+    public void testBold() {
+        String s1 = Utils.bold("test");
+
+        Assert.assertEquals("<b>test</b>", s1);
+    }
+
+    @Test
+    public void testReverseHyphenatedString() {
+        String s1 = Utils.reverseHyphenatedString("04-05-2020");
+
+        Assert.assertEquals("2020-05-04", s1);
+    }
+
+    @Test
+    public void testGetDate() {
+        Date dt1 = Utils.getDate("2020-05-05 11:46:00");
+        Date dt2 = Utils.getDate("20200505997782329");
+
+        Assert.assertNotNull(dt1);
+        Assert.assertNull(dt2);
+    }
+
+    @Test
+    public void testFormatAtBirthKey() {
+        String s1 = Utils.formatAtBirthKey("birth");
+        String s2 = Utils.formatAtBirthKey("Birth");
+        String s3 = Utils.formatAtBirthKey("test");
+
+        Assert.assertEquals("at_birth", s1);
+        Assert.assertEquals("at_Birth", s2);
+        Assert.assertEquals("test", s3);
     }
 }
