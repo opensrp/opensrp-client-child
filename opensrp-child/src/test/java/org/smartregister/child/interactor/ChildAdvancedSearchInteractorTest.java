77a5960445c6b66882609e959f4b58dea11f111d
Rodgers Andati <andatirodgers@gmail.com>
2020-06-16 21:22:53 +0300

    test ChildAdvancedSearchInteractor

---
+++ opensrp-child/src/test/java/org/smartregister/child/interactor/ChildAdvancedSearchInteractorTest.java
@@ -0,0 +1,97 @@
+package org.smartregister.child.interactor;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.smartregister.Context;
+import org.smartregister.CoreLibrary;
+import org.smartregister.DristhiConfiguration;
+import org.smartregister.child.util.AppExecutors;
+import org.smartregister.child.util.Constants;
+import org.smartregister.domain.Response;
+import org.smartregister.domain.ResponseStatus;
+import org.smartregister.service.HTTPAgent;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CoreLibrary.class})
+public class ChildAdvancedSearchInteractorTest {
+    @Mock
+    private CoreLibrary coreLibrary;
+
+    @Mock
+    DristhiConfiguration dristhiConfiguration;
+
+    @Mock
+    Context context;
+
+    @Mock
+    HTTPAgent httpAgent;
+
+    private ChildAdvancedSearchInteractor childAdvancedSearchInteractor;
+
+    @Before
+    public void setUp() {
+        childAdvancedSearchInteractor = new ChildAdvancedSearchInteractor(Mockito.mock(AppExecutors.class));
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testEnchanceStatusFilter() throws Exception {
+        Method enchanceStatusFilterMethod = ChildAdvancedSearchInteractor.class.getDeclaredMethod("enchanceStatusFilter", Map.class);
+        enchanceStatusFilterMethod.setAccessible(true);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "test");
+        enchanceStatusFilterMethod.invoke(childAdvancedSearchInteractor, map);
+
+        Assert.assertEquals(map.get(Constants.CHILD_STATUS.ACTIVE), "false");
+
+        Assert.assertEquals(map.get(Constants.CHILD_STATUS.LOST_TO_FOLLOW_UP), "false");
+    }
+
+    @Test
+    public void testUrlEncode() throws Exception {
+        Method urlEncodeMethod = ChildAdvancedSearchInteractor.class.getDeclaredMethod("urlEncode", String.class);
+        urlEncodeMethod.setAccessible(true);
+
+        Assert.assertEquals("key%3Dvalue", urlEncodeMethod.invoke(childAdvancedSearchInteractor, "key=value"));
+    }
+
+    @Test
+    public void testGlobalSearch() throws Exception {
+        Method globalSearchMethod = ChildAdvancedSearchInteractor.class.getDeclaredMethod("globalSearch", Map.class);
+        globalSearchMethod.setAccessible(true);
+
+        PowerMockito.mockStatic(CoreLibrary.class);
+        PowerMockito.when(CoreLibrary.getInstance()).thenReturn(coreLibrary);
+        PowerMockito.when(coreLibrary.context()).thenReturn(context);
+        PowerMockito.when(context.configuration()).thenReturn(dristhiConfiguration);
+
+        ChildAdvancedSearchInteractor interactor = spy(ChildAdvancedSearchInteractor.class);
+        PowerMockito.when(interactor.getHttpAgent()).thenReturn(httpAgent);
+
+        Response<String> resp = new Response<>(ResponseStatus.success, "success");
+        PowerMockito.when(httpAgent.fetch(anyString())).thenReturn(resp);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "test");
+        globalSearchMethod.invoke(childAdvancedSearchInteractor, map);
+
+        verify(interactor).getHttpAgent();
+    }
+}
