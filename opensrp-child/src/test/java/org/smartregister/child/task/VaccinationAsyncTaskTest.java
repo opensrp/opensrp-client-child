d2fcca1e92723c5f53d50c1dfff977636159760d
Rodgers Andati <andatirodgers@gmail.com>
2020-08-19 10:36:12 +0300

    additional tests

--- opensrp-child/src/test/java/org/smartregister/child/task/VaccinationAsyncTaskTest.java
+++ opensrp-child/src/test/java/org/smartregister/child/task/VaccinationAsyncTaskTest.java
@@ -1,10 +1,16 @@
 package org.smartregister.child.task;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
@@ -12,30 +18,55 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.child.BaseUnitTest;
 import org.smartregister.child.ChildLibrary;
+import org.smartregister.child.R;
 import org.smartregister.child.domain.RegisterActionParams;
 import org.smartregister.child.util.Constants;
 import org.smartregister.child.util.Utils;
+import org.smartregister.child.wrapper.VaccineViewRecordUpdateWrapper;
+import org.smartregister.domain.Alert;
+import org.smartregister.domain.AlertStatus;
 import org.smartregister.immunization.ImmunizationLibrary;
 import org.smartregister.immunization.domain.GroupVaccineCount;
 import org.smartregister.immunization.domain.jsonmapping.Vaccine;
 import org.smartregister.immunization.domain.jsonmapping.VaccineGroup;
 import org.smartregister.immunization.util.VaccineCache;
-import org.smartregister.util.AppProperties;
+import org.smartregister.util.AppProperties;;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Ephraim Kigamba - ekigamba@ona.io on 2019-09-06
  */
 
 public class VaccinationAsyncTaskTest extends BaseUnitTest {
+
+    @Mock
+    private VaccineViewRecordUpdateWrapper vaccineViewRecordUpdateWrapper;
+
+    @Mock
+    private View view;
+
+    @Mock
+    private TextView textView;
+
+    @Mock
+    private ImageView imageView;
+
+    @Mock
+    private LinearLayout linearLayout;
+
+    @Mock
+    private RegisterActionParams registerActionParams;
 
     private VaccinationAsyncTask vaccinationAsyncTask;
     private ImmunizationLibrary immunizationLibrary;
@@ -72,7 +103,7 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
 
         vaccineRepoMap.put("vaccines.json", Arrays.asList(new VaccineGroup[]{vaccineGroup}));
 
-        Mockito.when(immunizationLibrary.getVaccinesConfigJsonMap()).thenReturn(vaccineRepoMap);
+        when(immunizationLibrary.getVaccinesConfigJsonMap()).thenReturn(vaccineRepoMap);
 
         HashMap<String, VaccineCache> vaccineCacheHashMap = new HashMap<>();
 
@@ -97,7 +128,9 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
         Mockito.doReturn(appProperties).when(childLibrary).getProperties();
         Mockito.doReturn(false).when(appProperties).hasProperty(Mockito.anyString());
 
-        vaccinationAsyncTask = new VaccinationAsyncTask(Mockito.mock(RegisterActionParams.class)
+        when(registerActionParams.getConvertView()).thenReturn(view);
+
+        vaccinationAsyncTask = new VaccinationAsyncTask(registerActionParams
                 , null
                 , null
                 , null
@@ -197,4 +230,215 @@ public class VaccinationAsyncTaskTest extends BaseUnitTest {
         message = (String) getAlertMessage.invoke(vaccinationAsyncTask, VaccinationAsyncTask.State.FULLY_IMMUNIZED, "opv0");
         assertEquals("Done", message);
     }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenChildIsFullyImmunized() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(null);
+
+        List<org.smartregister.immunization.domain.Vaccine> vaccines = new LinkedList<>();
+        org.smartregister.immunization.domain.Vaccine vaccine = new org.smartregister.immunization.domain.Vaccine();
+        vaccine.setName("testvaccine");
+        vaccines.add(vaccine);
+        when(vaccineViewRecordUpdateWrapper.getVaccines()).thenReturn(vaccines);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText(R.string.fully_immunized_label);
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenChildIsInactive() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("true");
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText(R.string.inactive);
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenChildIsLostToFollowUp() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("true");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText(R.string.lost_to_follow_up_with_nl);
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsNoAlert() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setTextColor(RuntimeEnvironment.application.getResources().getColor(R.color.client_list_grey));
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsDue() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        Map<String, Object> nv = new HashMap<>();
+        Alert alert = new Alert("caseID", "scheduleName", "visitCode", AlertStatus.normal, "startDate", "expiryDate", true);
+        nv.put(Constants.KEY.ALERT, alert);
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(nv);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText("\nDue");
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsOverdue() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        Map<String, Object> nv = new HashMap<>();
+        Alert alert = new Alert("caseID", "scheduleName", "visitCode", AlertStatus.urgent, "startDate", "expiryDate", true);
+        nv.put(Constants.KEY.ALERT, alert);
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(nv);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText("\nOverdue");
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsExpired() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        Map<String, Object> nv = new HashMap<>();
+        Alert alert = new Alert("caseID", "scheduleName", "visitCode", AlertStatus.expired, "startDate", "expiryDate", true);
+        nv.put(Constants.KEY.ALERT, alert);
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(nv);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText(R.string.expired_label);
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsUpcoming() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        Map<String, Object> nv = new HashMap<>();
+        Alert alert = new Alert("caseID", "scheduleName", "visitCode", AlertStatus.upcoming, "startDate", "expiryDate", true);
+        nv.put(Constants.KEY.ALERT, alert);
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(nv);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText("Upcoming\n");
+    }
+
+    @Test
+    public void testUpdateRecordVaccinationWhenStateIsWaiting() throws Exception {
+        Method updateRecordVaccination = VaccinationAsyncTask.class.getDeclaredMethod("updateRecordVaccination", VaccineViewRecordUpdateWrapper.class);
+        updateRecordVaccination.setAccessible(true);
+
+        when(vaccineViewRecordUpdateWrapper.getConvertView()).thenReturn(view);
+        when(vaccineViewRecordUpdateWrapper.getLostToFollowUp()).thenReturn("false");
+        when(vaccineViewRecordUpdateWrapper.getInactive()).thenReturn("false");
+
+        when(vaccineViewRecordUpdateWrapper.getNv()).thenReturn(null);
+
+        when(view.findViewById(R.id.record_vaccination)).thenReturn(view);
+        when(view.findViewById(R.id.record_vaccination_text)).thenReturn(textView);
+        when(view.findViewById(R.id.record_vaccination_check)).thenReturn(imageView);
+        when(view.findViewById(R.id.record_vaccination_harvey_ball)).thenReturn(imageView);
+        when(imageView.getParent()).thenReturn(linearLayout);
+
+        updateRecordVaccination.invoke(vaccinationAsyncTask, vaccineViewRecordUpdateWrapper);
+
+        verify(textView).setText(R.string.waiting_label);
+    }
+
+
 }
