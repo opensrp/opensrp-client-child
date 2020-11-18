407756a5d48e8deb62c9668928ac2f1049ae4b70
Martin Ndegwa <ndegwamartin@gmail.com>
2020-11-18 10:16:42 +0300

    clean up unit tests ✅

--- opensrp-child/src/test/java/org/smartregister/child/fragment/ChildFormFragmentTest.java
+++ opensrp-child/src/test/java/org/smartregister/child/fragment/ChildFormFragmentTest.java
@@ -23,7 +23,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
