648a0e4a90b09357214d6ed1f31a09f5dd45c7bc
Elly Kitoto <junkmailstoelly@gmail.com>
2020-11-23 12:32:58 +0300

    Update client attributes when lost card reported

--- opensrp-child/src/main/java/org/smartregister/child/presenter/BaseChildDetailsPresenter.java
+++ opensrp-child/src/main/java/org/smartregister/child/presenter/BaseChildDetailsPresenter.java
@@ -1,15 +1,31 @@
 package org.smartregister.child.presenter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.AllConstants;
+import org.smartregister.child.ChildLibrary;
 import org.smartregister.child.contract.ChildTabbedDetailsContract;
+import org.smartregister.repository.EventClientRepository;
 
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+import timber.log.Timber;
+
 public class BaseChildDetailsPresenter implements ChildTabbedDetailsContract.Presenter {
 
+    public static final String CARD_STATUS = "card_status";
+    public static final String CARD_STATUS_DATE = "card_status_date";
+
+    public enum CardStatus {
+        needs_card, does_not_need_card
+    }
+
     private final WeakReference<ChildTabbedDetailsContract.View> view;
+    private final EventClientRepository eventClientRepository = ChildLibrary.getInstance().eventClientRepository();
 
     public BaseChildDetailsPresenter(ChildTabbedDetailsContract.View view) {
         this.view = new WeakReference<>(view);
@@ -23,8 +39,25 @@ public class BaseChildDetailsPresenter implements ChildTabbedDetailsContract.Pre
     @Override
     public void reportLostCard(String baseEntityId) {
         if (getView() != null) {
-            String orderDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).format(new Date());
-            getView().notifyLostCardReported(orderDate);
+            JSONObject client = eventClientRepository.getClientByBaseEntityId(baseEntityId);
+            try {
+
+                JSONObject clientAttributes = client.getJSONObject(AllConstants.ATTRIBUTES);
+                Date currentTime = Calendar.getInstance().getTime();
+                clientAttributes.put(CARD_STATUS, CardStatus.needs_card.name());
+                clientAttributes.put(CARD_STATUS_DATE, currentTime);
+
+                client.put(AllConstants.ATTRIBUTES, clientAttributes);
+
+                eventClientRepository.addorUpdateClient(baseEntityId, client);
+
+                String orderDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+                        .format(currentTime);
+                getView().notifyLostCardReported(orderDate);
+
+            } catch (JSONException e) {
+                Timber.e(e);
+            }
         }
     }
 }
