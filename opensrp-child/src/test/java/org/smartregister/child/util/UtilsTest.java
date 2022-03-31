772995ead9d31349cbd0471e76015c2ba294b360
Simon Njoroge <snjoroge@ona.io>
2022-03-31 12:52:43 +0300

    Update unit test

--- opensrp-child/src/test/java/org/smartregister/child/util/UtilsTest.java
+++ opensrp-child/src/test/java/org/smartregister/child/util/UtilsTest.java
@@ -392,7 +392,7 @@ public class UtilsTest {
 
     @Test
     public void testDobStringToDateReturnsNullWhenDobStringIsInvalidDateFormat() {
-        Assert.assertNull(Utils.dobStringToDate("20211-12-12"));
+        Assert.assertNull(Utils.dobStringToDate("20211-31-02"));
     }
 
     @Test
