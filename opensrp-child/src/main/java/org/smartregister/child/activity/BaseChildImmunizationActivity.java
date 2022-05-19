9aa68423992508c07ad3ab79c929ee8fdd23d7ad
Simon Njoroge <snjoroge@ona.io>
2022-05-19 15:39:30 +0300

    Prevent duplicate growth chart dialogs from loading

--- opensrp-child/src/main/java/org/smartregister/child/activity/BaseChildImmunizationActivity.java
+++ opensrp-child/src/main/java/org/smartregister/child/activity/BaseChildImmunizationActivity.java
@@ -1354,8 +1354,6 @@ public abstract class BaseChildImmunizationActivity extends BaseChildActivity
             RecordGrowthDialogFragment recordWeightDialogFragment = RecordGrowthDialogFragment.newInstance(dob, weightWrapper, heightWrapper);
             recordWeightDialogFragment.show(fragmentTransaction, DIALOG_TAG);
         }
-
-
     }
 
     @Override
@@ -1679,9 +1677,11 @@ public abstract class BaseChildImmunizationActivity extends BaseChildActivity
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         Fragment prev = getSupportFragmentManager().findFragmentByTag(BaseChildImmunizationActivity.DIALOG_TAG);
         if (prev != null) {
-            fragmentTransaction.remove(prev);
+            return;
         }
         fragmentTransaction.addToBackStack(null);
+
+        showProgressDialog(getString(R.string.loading), getString(R.string.loading_form_message));
 
         List<Weight> weights = new ArrayList<>();
         List<Height> heights = new ArrayList<>();
@@ -1689,7 +1689,6 @@ public abstract class BaseChildImmunizationActivity extends BaseChildActivity
         if (growthMonitoring == null || growthMonitoring.isEmpty()) {
             Utils.showToast(this, getString(R.string.record_growth_details));
         } else {
-
             if (growthMonitoring.containsKey(Constants.WEIGHT)) {
                 weights = growthMonitoring.get(Constants.WEIGHT);
             }
@@ -1697,18 +1696,17 @@ public abstract class BaseChildImmunizationActivity extends BaseChildActivity
             if (growthMonitoring.containsKey(Constants.HEIGHT)) {
                 heights = growthMonitoring.get(Constants.HEIGHT);
             }
-
-
         }
 
         GrowthDialogFragment growthDialogFragment = GrowthDialogFragment.newInstance(childDetails, weights, heights);
         growthDialogFragment.show(fragmentTransaction, BaseChildImmunizationActivity.DIALOG_TAG);
+
+        hideProgressDialog();
     }
 
     ////////////////////////////////////////////////////////////////
     // Inner classes
     ////////////////////////////////////////////////////////////////
-
 
     public void updateScheduleDate() {
         String dobString = Utils.getValue(childDetails.getColumnmaps(), Constants.KEY.DOB, false);
