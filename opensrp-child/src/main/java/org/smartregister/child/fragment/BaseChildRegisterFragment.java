e998c89a61b3b4f03d0b977eebc55b286473900b
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2022-07-28 13:57:04 +0500

    Fix filter button visibility

--- opensrp-child/src/main/java/org/smartregister/child/fragment/BaseChildRegisterFragment.java
+++ opensrp-child/src/main/java/org/smartregister/child/fragment/BaseChildRegisterFragment.java
@@ -461,7 +461,6 @@ public abstract class BaseChildRegisterFragment extends BaseRegisterFragment
                             Timber.i("Total Register Count %d", clientAdapter.getTotalcount());
                             clientAdapter.setCurrentlimit(20);
                             clientAdapter.setCurrentoffset(0);
-
                             // Update filter visibility and count once overDueCount is calculated
                             updateDueOverdueCountText();
                         }
