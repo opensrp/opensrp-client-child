a6a8a59e716837f3cd2a80b8d4f1f612c65eb416
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2022-08-15 16:26:43 +0500

    Code cleanup

--- opensrp-child/src/main/java/org/smartregister/child/fragment/BaseChildRegisterFragment.java
+++ opensrp-child/src/main/java/org/smartregister/child/fragment/BaseChildRegisterFragment.java
@@ -482,13 +482,6 @@ public abstract class BaseChildRegisterFragment extends BaseRegisterFragment
 
                     int totalCount = commonRepository().countSearchIds(sql);
 
-                    // For overdue count
-                    /*// FIXME: Count generated on first sync is not correct
-                    String sqlOverdueCount = Utils.metadata().getRegisterQueryProvider()
-                            .getCountExecuteQuery(filterSelectionCondition(true), "");
-
-                    overDueCount = commonRepository().countSearchIds(sqlOverdueCount);
-                    Timber.i("Total Overdue Count %d ", overDueCount);*/
                     executors.mainThread().execute(new Runnable() {
                         @Override
                         public void run() {
@@ -536,9 +529,6 @@ public abstract class BaseChildRegisterFragment extends BaseRegisterFragment
             public void run() {
                 Timber.i("Started running the overdue count query");
 
-//                String sqlOverdueCount = Utils.metadata().getRegisterQueryProvider()
-//                        .getCountExecuteQuery(filterSelectionCondition(true), "");
-//                int overDueCount = commonRepository().countSearchIds(sqlOverdueCount);
                 int overDueCount = VaccineOverdueCountRepositoryHelper.getOverdueCount();
                 setOverDueCount(overDueCount);
 
